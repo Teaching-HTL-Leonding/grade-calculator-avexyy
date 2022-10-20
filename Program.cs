ff01e12a2043febdb57c468fec77313de1125377
avexyy <113837032+avexyy@users.noreply.github.com>
2022-10-20 12:25:22 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿int possiblePoints = 40;
+﻿int possiblePoints = 80;
 double percentage  = 0;
 int totalPoints = 0;
 int grade = 0;
@@ -23,7 +23,20 @@ if(decision == "yes" || decision == "YES")
 }
 else{possiblePoints -= 20;}
 
-percentage = 100d* totalPoints / possiblePoints;
+System.Console.Write("How many points(0-20) did you get through homeworks? ");
+totalPoints += int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Did you have an oral exam? (yes/no) ");
+decision = Console.ReadLine()!;
+
+if(decision == "yes" || decision == "YES")
+{
+    System.Console.Write("How many points(0-20) did you get for the oral exam? ");
+    totalPoints += int.Parse(Console.ReadLine()!);
+}
+else{possiblePoints -= 20;}
+
+percentage = 100d * totalPoints / possiblePoints;
 
 if(percentage >= 89) {grade = 1;}
 else if(percentage >= 76) {grade = 2;}
